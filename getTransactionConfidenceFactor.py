35098ed955a97a5552ca1531f8eeb88f2874ffe1
Kevin Zhao <kevinzhao53@gmail.com>
2015-12-04 18:49:09 -0500

    updated get confidence factor to use url
    no longer using blockcypher Python API since the bitcoin fold issue is too confusing

--- getTransactionConfidenceFactor.py
+++ getTransactionConfidenceFactor.py
@@ -1,9 +1,9 @@
-import blockcypher
+import requests
 
 #use this method to get the transaction confidence factor given the hash of the transaction
 def getTransactionConfidenceFactor(txID):
-    transaction = blockcypher.get_transaction_details(txID, api_key='d78e7b0f0c88597200452313a1c32fc9')
-    print(transaction['confidence'])
+    r = requests.get('https://api.blockcypher.com/v1/btc/main/txs/' + txID)
+    print((r.json())['confidence'])
     if 'receive_count' in transaction:
     	print(transaction['receive_count'])
     else:
@@ -12,3 +12,5 @@ def getTransactionConfidenceFactor(txID):
 if __name__ == "__main__":
 	getTransactionConfidenceFactor("c52e036fd70785415f5b205e7a472b7538e0d6c4a3c14edc106183068089c279")
 	getTransactionConfidenceFactor("42e039a611fc4bcc1b0b991853ebd82f4827723352238c91a61ff39660c0531c")
+
+
